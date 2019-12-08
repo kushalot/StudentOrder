eb5a44d6d2dd0dcd8d0e39d9a018d3b01e4e9cbf
kushalot <38114239+kushalot@users.noreply.github.com>
2019-12-08 16:01:00 +0300

    1

---
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -0,0 +1,40 @@
+package edu.javacourse.studentorder;
+
+import edu.javacourse.studentorder.domain.Adult;
+import edu.javacourse.studentorder.domain.Person;
+import edu.javacourse.studentorder.domain.StudentOrder;
+
+public class SaveStudentOrder {
+    public static void main(String[] args) {
+        buildStudentOrder();
+
+//        StudentOrder so = new StudentOrder();
+  //      long ans = saveStudentOrder(so);
+    //    System.out.println(ans);
+
+    }
+
+    static long saveStudentOrder(StudentOrder studentOrder){
+        long answer = 199;
+        System.out.println("saveStudentOrder: ");
+
+        return answer;
+    }
+
+   public static StudentOrder buildStudentOrder(){
+        StudentOrder so = new StudentOrder();
+
+        Adult husband = new Adult();
+
+
+
+//        husband.setGivenName("Slava");
+//        husband.setSurName("Kushner");
+//        husband.setPassportNumber("123456");
+//        so.setHusband(husband);
+
+//        String ans = husband.getPersonString();
+//        System.out.println(ans);
+        return so;
+    }
+}
