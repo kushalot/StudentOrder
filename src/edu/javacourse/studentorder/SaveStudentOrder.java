0df9ce9efb4e64f9a737aa3b0dbf8cab8bc24185
kushalot <38114239+kushalot@users.noreply.github.com>
2020-01-13 18:53:01 +0300

    list

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -11,16 +11,7 @@ import java.time.LocalDate;
 
 public class SaveStudentOrder {
     public static void main(String[] args) throws Exception {
-       // Class.forName("org.postgresql.Driver");
-        Connection con = DriverManager.getConnection(
-            "jdbc:postgresql://localhost:5432/jc_student",
-                "postgres", "postgres");
 
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM jc_street");
-        while (rs.next()){
-            System.out.println(rs.getLong(1) + " : " + rs.getString(2));
-        }
 
 //        StudentOrder s = buildStudentOrder(10);
 //        StudentOrder so = new StudentOrder();
@@ -43,7 +34,9 @@ public class SaveStudentOrder {
        so.setMarriageDate(LocalDate.of(2016, 7, 4));
        so.setMarriageOffice("Отдел ЗАГС");
 
-       Address address = new Address("195000", "Заневский пр.", "12", "", "142");
+       Street street = new Street(1L, "First Street");
+
+       Address address = new Address("195000", street, "12", "", "142");
 
        // Муж
        Adult husband = new Adult("Петров", "Виктор", "Сергеевич", LocalDate.of(1997, 8, 24));
